3cc480a9c18a1719d082ff88d6bcb58af21a10df
Liam Pulles <me@liampulles.com>
2020-06-06 18:10:21 +0200

    Vue base

--- rikyu-ui/.eslintrc.js
+++ rikyu-ui/.eslintrc.js
@@ -1,25 +1,29 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es6: true,
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
-    'airbnb-base',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    ecmaVersion: 2020,
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
 };
